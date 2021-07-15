9fc5be76ef6a8d7d43983c5f9bcb31380849dca4
Michal Cizek <xcizek3@fi.muni.cz>
2021-07-15 23:39:20 +0200

    added react-hook-form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,21 +3,37 @@ import "./Header.css";
 
 import BEMHelper from "react-bem-helper";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import { useForm } from "react-hook-form";
 import { WhiteButton, WhiteTextField } from "./StyledInputs";
 
 const classes = new BEMHelper("header");
 
-const Header = () => (
-  <header {...classes()}>
-    <div {...classes("title")}>
-      <GitHubIcon fontSize="large" />
-      <h1 {...classes("heading")}>GitHub-Viewer</h1>
-    </div>
-    <form {...classes("search")}>
-      <WhiteTextField variant="outlined" label="username" size="small" />
-      <WhiteButton variant="outlined">Search</WhiteButton>
-    </form>
-  </header>
-);
+const Header = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <header {...classes()}>
+      <div {...classes("title")}>
+        <GitHubIcon fontSize="large" />
+        <h1 {...classes("heading")}>GitHub-Viewer</h1>
+      </div>
+      <form {...classes("search")} onSubmit={onSubmit}>
+        <WhiteTextField
+          variant="outlined"
+          label="username"
+          size="small"
+          {...register("username")}
+        />
+        <WhiteButton variant="outlined" type="submit">
+          Search
+        </WhiteButton>
+      </form>
+    </header>
+  );
+};
 
 export default Header;
