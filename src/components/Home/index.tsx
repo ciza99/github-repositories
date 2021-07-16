62f1636e6047f87bb4a5218ba93c106852283cd9
Michal Cizek <xcizek3@fi.muni.cz>
2021-07-17 00:25:53 +0200

    added footer to homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,6 +5,7 @@ import BEMHelper from "react-bem-helper";
 import SearchIcon from "@material-ui/icons/Search";
 import FolderOpenIcon from "@material-ui/icons/FolderOpen";
 import DomainIcon from "@material-ui/icons/Domain";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import FeatureCard from "../FeatureCard";
 
 const classes = new BEMHelper("home");
@@ -30,8 +31,18 @@ const Home = () => (
       <FeatureCard
         Icon={DomainIcon}
         title="View organisations"
-        text="Find information about the users organisations."
+        text="Find users organizations and link to more information."
       />
+    </div>
+    <div {...classes("footer")}>
+      <p {...classes("copyright")}>Copyright © 2021 Michal Čížek</p>
+      <GitHubIcon {...classes("icon")} fontSize="large" />
+      <a
+        {...classes("link")}
+        href="https://github.com/ciza99/github-repositories"
+      >
+        github repository for this project
+      </a>
     </div>
   </div>
 );
