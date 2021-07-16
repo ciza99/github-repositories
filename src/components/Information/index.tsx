79cf0cae751c69ccd28aaccce65cf0e01f927c51
Michal Cizek <xcizek3@fi.muni.cz>
2021-07-16 23:10:09 +0200

    added more information about users

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -7,6 +7,11 @@ import FolderOpenOutlinedIcon from "@material-ui/icons/FolderOpenOutlined";
 import EventOutlinedIcon from "@material-ui/icons/EventOutlined";
 import { useRecoilValue } from "recoil";
 import moment from "moment";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import BusinessOutlinedIcon from "@material-ui/icons/BusinessOutlined";
 import InfoCard from "../InfoCard";
 import InfoItem from "../InfoItem";
 import { userState } from "../../atoms/user";
@@ -43,9 +48,33 @@ const Information = () => {
           text={`${user.public_repos} public repositories`}
         />
       </InfoCard>
-      <InfoCard title="General">
+      <InfoCard title="Timeline">
         <InfoItem Icon={EventOutlinedIcon} text={`created at ${createdAt}`} />
         <InfoItem Icon={EventOutlinedIcon} text={`updated at ${updatedAt}`} />
+      </InfoCard>
+      <InfoCard title="Socials">
+        <InfoItem
+          Icon={TwitterIcon}
+          text={user.twitter_username || "twitter not specified"}
+        />
+        <InfoItem
+          Icon={MailOutlineIcon}
+          text={user.email || "email not specified"}
+        />
+      </InfoCard>
+      <InfoCard title="General">
+        <InfoItem
+          Icon={InfoOutlinedIcon}
+          text={user.bio || "bio not specified"}
+        />
+        <InfoItem
+          Icon={LocationOnIcon}
+          text={user.location || "location not specified"}
+        />
+        <InfoItem
+          Icon={BusinessOutlinedIcon}
+          text={user.company || "company not specified"}
+        />
       </InfoCard>
     </div>
   );
